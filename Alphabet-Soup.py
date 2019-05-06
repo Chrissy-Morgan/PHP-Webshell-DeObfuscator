8085616135afb4e265290c36dfbc34d8864bec0f
Chrissy Morgan <43700993+Chrissy-Morgan@users.noreply.github.com>
2019-05-07 00:11:35 +0100

    Update Alphabet-Soup.py
    Amendment to $GLOBALS spacing

--- Alphabet-Soup.py
+++ Alphabet-Soup.py
@@ -8,7 +8,7 @@ import re
 
 #Key
 
-Alphabet="ENTER YOUR ALPHABET HERE"
+Alphabet="\x59\x4f\x45\x54\x38\x3a\x23\x30\x24\x40\x3b\x7c\x2f\x37\x66\x42\x09\x35\x72\x43\x0a\x2a\x2e\x4c\x29\x6f\x2d\x53\x4e\x44\x34\x5b\x41\x4a\x33\x74\x68\x76\x4d\x3e\x60\x36\x26\x6b\x67\x56\x20\x32\x7e\x22\x7a\x61\x70\x28\x58\x6a\x27\x57\x71\x39\x25\x51\x46\x7d\x48\x5f\x5e\x73\x0d\x79\x2c\x62\x75\x5a\x78\x21\x2b\x4b\x63\x6c\x31\x50\x3f\x77\x47\x6e\x69\x3c\x64\x49\x6d\x65\x5d\x52\x3d\x5c\x7b\x55"
 # this is the first part of obfuscated code we need to extract the numbers from
 
 obs = []
@@ -68,7 +68,7 @@ with open('globals.php') as infile:
       string = string.translate(None, ''.join(bad_chars)) 
       #Add new lines and spaces for formatting
       string = re.sub(";", ";\n", string)
-      string = re.sub("\$([GLOBALS]\w+)", "$GLOBALS ", string)
+      string = re.sub("\$GLOBALS", "$GLOBALS ", string)
       print ("Deobfuscated Code : " + string) 
       #Output to file
       f = open("output.txt","a+")
